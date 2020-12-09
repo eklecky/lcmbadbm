5a36eadd195d94fd0c6b694bd1da2791547d36e3
Efraim Klecky <efraim@klecky.org>
2020-12-08 19:30:55 -0500

    Added non Swing compliant Ui (This should have been done in the last BadBm refactoring)

---
+++ src/main/java/edu/touro/mco152/bm/NonSwingDiskWorker.java
@@ -0,0 +1,38 @@
+package edu.touro.mco152.bm;
+
+import java.beans.PropertyChangeListener;
+
+public class NonSwingDiskWorker implements UIBluePrint{
+
+    boolean isProgressComplete;
+
+    public void provideProgress(int x) {
+       if(x < 100 ){
+            isProgressComplete = false;
+        }
+       else isProgressComplete = true;
+
+    }
+
+    public boolean cancelled() {
+        return false;
+    }
+
+
+    public boolean setCancel(boolean bool) {
+        return false;
+    }
+
+    public void newAddPropertyChangeListener(PropertyChangeListener listener) {
+        System.out.println(listener);
+    }
+
+    public void NowExecute() {
+
+    }
+
+    public void newPublish(DiskMark diskMark) {
+        System.out.println(diskMark);
+
+    }
+}
